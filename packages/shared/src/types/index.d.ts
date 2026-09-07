954fe326d2055f3756345ca53b5fb31d452d8571
FranciscoHonorat <jeffhonorato230@gmail.com>
2026-09-07 19:49:34 -0300

    feat: remove unused files and update dependencies

--- packages/shared/src/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Types index - Re-export all type definitions
- */
-export * from './metricTypes';
-export * from './alertTypes';
-export * from './configurationTypes';
-//# sourceMappingURL=index.d.ts.map
